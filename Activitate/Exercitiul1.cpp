226910bf726b0cad2a81fec318431e16e6961d92
Simona Dumitru <simonaionela.d@gmail.com>
2024-11-09 21:38:29 +0200

    update

--- Activitate/Exercitiul1.cpp
+++ Activitate/Exercitiul1.cpp
@@ -3,27 +3,68 @@
 
 using namespace std;
 
-//exercitiu facem soft - Bogdan Iancu
+//exercitiu facem soft - Bogdan Iancu - parcurgere cursuri Youtube
 class TelefonMobil {
 public:
 	int nivelBaterie;
 	int id;
 
-	void incarca(int nivelIncarcare) {
+	TelefonMobil() //contructor
+	{
+		producator = "";
+		model = "";
+		nivelBaterie = 0;
+	}
+
+	TelefonMobil(string _producator, string _model) //constructor cu parametri
+	{
+		producator = _producator;
+		model = _model;
+		nivelBaterie = 0;
+	}
+
+	TelefonMobil(int _nivelBaterie) //constrructor cu un singur parametru
+	{
+		nivelBaterie = _nivelBaterie;
+	}
+
+	void afisare() //functie de afisare
+	{
+		cout << "Producator: " << this->producator << endl;
+		cout << "Model: " << this->model << endl;
+		cout << "nivelBaterie:" << this->nivelBaterie << endl;
+	}
+
+	void incarca(int nivelIncarcare)
+	{
 		nivelBaterie += nivelIncarcare;
 	}
 protected:
 	string producator;
+private:
 	string model;
 };
 
 int main() {
 	TelefonMobil telefonPersonal;
-	TelefonMobil telefonDeServiciu;
+	TelefonMobil* pTelefon = new TelefonMobil(); 
+	delete pTelefon;
+	pTelefon = nullptr;
+
+	TelefonMobil telefonDeServiciu("Samsung", "S20");
+	telefonDeServiciu.afisare();
+
+	pTelefon = new TelefonMobil("Apple", "Iphone 14");
+	pTelefon->afisare();
+	delete pTelefon;
+	pTelefon = nullptr;
+
+	TelefonMobil telefon3(25);
+	TelefonMobil telefon3 = 30;
 
 	telefonPersonal.nivelBaterie = 20;
 	telefonPersonal.incarca(50);
 
 	cout << telefonPersonal.nivelBaterie << endl;
-	
+
 }
--- Activitate/Exercitiul2.cpp
+++ Activitate/Exercitiul2.cpp
@@ -1,42 +1,43 @@
-#include<iostream>
-#include<string>
-
-using namespace std;
-
-class Persoana {
-private:
-	int* varsta;
-
-public: 
-	string nume;
-	char sex;
-	float inaltime;
-
-	Persoana() {
-		this->nume = "Nume";
-		this->sex = 'F/M';
-		this->inaltime = inaltime;
-		this->varsta = nullptr;
-	}
-
-	Persoana(string _nume, char _sex, float _inaltime, int* _varsta) {
-		this->nume = _nume;
-		this->sex = _sex;
-		this->inaltime = _inaltime;
-		this->varsta = _varsta; //asa nu..
-	}
-
-	void afisare() {
-		cout << "Nume: " << this->nume << endl;
-		cout << "Sex: " << this->sex << endl;
-		cout << "Inaltime:" << this->inaltime << endl;
-		cout << "Varsta: " << this->varsta << endl;
-	}
-
-};
-
-int main() {
-	Persoana persoana1;
-	persoana1.afisare();
-	Persoana persoana2;
-}
+//#include<iostream>
+//#include<string>
+//
+//using namespace std;
+//
+//class Persoana {
+//private:
+//	int* varsta;
+//
+//public: 
+//	string nume;
+//	char sex;
+//	float inaltime;
+//
+//	Persoana() {
+//		this->nume = "";
+//		this->sex = 'F/M';
+//		this->inaltime = inaltime;
+//		this->varsta = nullptr;
+//	}
+//
+//	Persoana(string _nume, char _sex, float _inaltime) {
+//		this->nume = _nume;
+//		this->sex = _sex;
+//		this->inaltime = _inaltime;
+//
+//	}
+//
+//	void afisare() {
+//		cout << "Nume: " << this->nume << endl;
+//		cout << "Sex: " << this->sex << endl;
+//		cout << "Inaltime:" << this->inaltime << endl;
+//		cout << "Varsta: " << this->varsta << endl;
+//	}
+//
+//};
+//
+//int main() {
+//	Persoana persoana1;
+//	persoana1.afisare();
+//	Persoana persoana2("M", 1, 7);
+//	persoana2.afisare();
+//}
